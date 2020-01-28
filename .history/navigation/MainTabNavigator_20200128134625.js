a517ce8f4852042193318f2a6f45f1183f5f5ec4
ph0ph0 <phillipalanlee@googlemail.com>
2020-01-28 13:46:33 +0000

    m

---
+++ .history/navigation/MainTabNavigator_20200128134625.js
@@ -0,0 +1,30 @@
+import React from "react";
+import { Platform } from "react-native";
+import {
+  createBottomTabNavigator,
+  createStackNavigator
+} from "react-navigation-stack";
+import TabBarIcon from "../components/TabBarIcon";
+import HomeScreen from "../screens/HomeScreen";
+
+const HomeStack = createStackNavigator({
+  Home: HomeScreen
+});
+
+HomeStack.navigationOptions = {
+  tabBarLabel: "Home",
+  tabBarIcon: ({ focused }) => (
+    <TabBarIcon
+      focused={focused}
+      name={
+        Platform.OS === "ios"
+          ? `ios-information-circle${focused ? "" : "-outline"}`
+          : "md-information-circle"
+      }
+    />
+  )
+};
+
+export default createBottomTabNavigator({
+  HomeStack
+});
